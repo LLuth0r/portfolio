103c613c9e70e0977e5da672a2ecf13455b41c03
Mike Pakulak <mike.pakulak@gmail.com>
2025-05-22 15:39:59 -0400

    Cleaned up redundant Header from app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import Header from "./components/Header/Header";
 import Home from "./screens/Home/Home";
 import SpaceGame from "./screens/SpaceGame/SpaceGame";
 
 function App() {
 	return (
 		<div className="app">
-			{/* <Header /> */}
 			<Routes>
 				<Route path="/" 
 							 element={ <Home /> }>
